5ddb46353e311acef2830c8731fd207ddf8ab79a
Seungwon Park <yyyyy@snu.ac.kr>
2018-09-28 21:06:16 +0900

    match text with lazy quantifier

--- snucsepl.py
+++ snucsepl.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import time
+import html
 from config import *
 
 def get_boardlist():
@@ -7,11 +9,11 @@ def get_boardlist():
 	req.encoding = 'utf-8'
 	prefix = '<td class="row1" width="100%"><span class="topictitle">'
 	suffix = '</a></span><span class="gensmall"><br />'
-	boardlist = re.findall(r'%s(.*)%s' % (prefix, suffix), req.text)
+	boardlist = re.findall(r'%s(.*?)%s' % (prefix, suffix), req.text)
 	post_info = []
 	for x in boardlist:
-		now_id = re.findall(r'viewtopic\.php\?t=(.*)&amp;', x)[0]
-		now_title = x.split('class="topictitle">')[1]
+		now_id = re.findall(r'viewtopic\.php\?t=(.*?)&amp;', x)[0]
+		now_title = html.unescape(x.split('class="topictitle">')[1])
 		post_info.append((now_id, now_title))
 	return post_info
 
@@ -19,14 +21,15 @@ def post(conn, post_id):
 	url = 'https://ropas.snu.ac.kr/phpbb/viewtopic.php?t=%s' % post_id
 	req = requests.get(url)
 	req.encoding = 'utf-8'
-	post_title = re.findall(r'<title> :: 주제 보기 - (.*)</title>', req.text)[0]
-	post_poster = re.findall(r'</a><b>(.*)</b></span><br /><span class="postdetails">', req.text)[0]
+	post_title = re.findall(r'<title> :: 주제 보기 - (.*?)</title>', req.text)[0]
+	post_poster = re.findall(r'</a><b>(.*?)</b></span><br /><span class="postdetails">', req.text)[0]
 	message = 'PL: %s | %s' % (post_title, post_poster)
 	conn.send(channel, message)
 	conn.send(channel, url)
 
 def update_boardlist(conn, boardlist_old):
 	boardlist_new = get_boardlist()
+	print('%s\t found %d post' % (time.strftime('%y/%m/%d %H:%M:%S'), len(boardlist_new)))
 	for x in boardlist_new:
 		if x not in boardlist_old:
 			post(conn, x[0])
